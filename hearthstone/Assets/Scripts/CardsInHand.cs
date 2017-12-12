0a95ac6cea8f53d85215e50365e54439b0450c1b
ColinvD <22950@ma-web.nl>
2017-12-12 13:52:32 +0100

    version 12-12-2017

---
+++ hearthstone/Assets/Scripts/CardsInHand.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsInHand : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> Cards;
+    private Transform rotationPoint;
+    private int oldCardsCount = 0;
+    private List<Vector3> cardRotations;
+    private List<Vector3> cardPositions;
+    
+	void Start()
+    {
+        cardRotations = new List<Vector3>();
+        cardPositions = new List<Vector3>();
+        rotationPoint = gameObject.transform;
+    }
+    
+    void Update()
+    {
+        if(Cards.Count != oldCardsCount)
+        {
+            if(cardRotations.Count > 0)
+            {
+                cardRotations.Clear();
+            }
+            if(cardRotations.Count > 0)
+            {
+                cardPositions.Clear();
+            }
+            for(int i = 0; i < Cards.Count; i++)
+            {
+                float yRot = (-(Cards.Count - 1.0f) / 2.0f + i) * 20;
+                float xPos = (-(Cards.Count - 1.0f) / 2.0f + i) * 2;
+                float zPos = 0;
+                cardRotations.Add(new Vector3(0, yRot, -20));
+                cardPositions.Add(new Vector3(rotationPoint.position.x + xPos, 3, rotationPoint.position.z + zPos));
+                Cards[i].transform.eulerAngles = cardRotations[i];
+                Cards[i].transform.position = cardPositions[i];
+            }
+            oldCardsCount = Cards.Count;
+        }
+	}
+
+    public Vector3 getCardRotations(string cardName)
+    {
+        int card = 0;
+        for(int i = 0; i < Cards.Count; i++)
+        {
+            if(cardName == Cards[i].name)
+            {
+                card = i;
+            }
+        }
+        return cardRotations[card];
+    }
+
+    public Vector3 getCardPositions(string cardName)
+    {
+        int card = 0;
+        for (int i = 0; i < Cards.Count; i++)
+        {
+            if (cardName == Cards[i].name)
+            {
+                card = i;
+            }
+        }
+        return cardPositions[card];
+    }
+}
---
+++ hearthstone/Assets/Scripts/CreateRayCast.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRayCast : MonoBehaviour
+{
+    private CardToMousePosition moveActions;
+    private CardsInHand cardsHand;
+    private ManaAmount manaAmount;
+    private CardData cardData;
+    private int cardMask;
+    private int playfieldMask;
+    private int defaultMask;
+
+    void Start()
+    {
+        manaAmount = FindObjectOfType<ManaAmount>();
+        cardsHand = FindObjectOfType<CardsInHand>();
+        cardMask = LayerMask.GetMask("Cards");
+        playfieldMask = LayerMask.GetMask("Playfield");
+        defaultMask = ~cardMask;
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+
+        if(Physics.Raycast(ray, out hit, 50f, defaultMask)) //everything else but not the cards
+        {
+            if(Input.GetMouseButtonDown(0) && hit.collider.tag == "EndTurnButton")
+            {
+                hit.collider.GetComponent<TurnSwitch>().ChangeTurn();
+            }
+            if(Input.GetMouseButtonUp(0) && hit.collider.name != "FightField")
+            {
+                if(moveActions != null)
+                {
+                    moveActions.gameObject.transform.position = cardsHand.getCardPositions(moveActions.gameObject.name);
+                    moveActions.gameObject.transform.eulerAngles = cardsHand.getCardRotations(moveActions.gameObject.name);
+                }
+            }
+        }
+
+        if(Physics.Raycast(ray, out hit, 50f, cardMask)) //selecting the card
+        {
+            if(Input.GetMouseButtonDown(0) && hit.collider.tag == "Card")
+            {
+                moveActions = hit.collider.GetComponent<CardToMousePosition>();
+                cardData = hit.collider.GetComponent<CardData>();
+            }
+        }
+
+        if(Physics.Raycast(ray, out hit, 50f, playfieldMask)) //move the card
+        {
+            if(moveActions != null && cardData != null)
+            {
+                if (cardData.GetManaCost() <= manaAmount.GetCurrentMana())
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        moveActions.FollowChange();
+                    }
+                    if (Input.GetMouseButton(0))
+                    {
+                        moveActions.ChangePosition(hit);
+                    }
+                    if (Input.GetMouseButtonUp(0))
+                    {
+                        moveActions.FollowChange();
+                        moveActions = null;
+                        cardData = null;
+                    }
+                }
+            }
+        }
+    }
+}
